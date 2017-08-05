eb7d37aa7fbcb734860a49a54312dbc64a5493bc
Veronica612015 <Veronica612015@users.noreply.github.com>
2017-08-04 19:38:13 -0500

    Add files via upload

---
+++ lab1a.c
@@ -0,0 +1,14 @@
+/*  Program to print part of Old MacDonald
+ *  Name:
+ *  Date:
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Old MacDonald had a farm ");
+    printf("Ee i ee i o \n");
+    return 0;
+    
+}
